8fda6262206ebde3fd5d7d88aba6367935edd95d
soul4rent <kyle.a.wadsworth@gmail.com>
2018-05-01 12:18:49 -0700

    minor optimization, only trys workarounds if doesn't work first time

--- ggscrape.py
+++ ggscrape.py
@@ -19,14 +19,29 @@ botnolikey = 0 #keep track of number of no-bot websites
 #get top 20 results for reddit
 print("when google searching "+searchterm+", these websites contain the phrase "+ctrlf+" somewhere on the page")
 for url in search(searchterm, stop=20):
-    time.sleep(5) #attempting to fix most "too many requests" errors
+
     try: #if the website hates robots
         f = urllib.request.urlopen(url)
         webdata = f.read()
         if (ctrlf.encode("utf-8") in webdata): #ctrl-f the page
             print(url)
-    except:
-        botnolikey = botnolikey+1
+
+
+    except: #if there is ANY error
+
+        
+        for x in range (0,3): #try it again 3 times, see what happens.
+            try:
+                time.sleep(3) #attempting to fix most "too many requests" errors
+                f = urllib.request.urlopen(url)
+                webdata = f.read()
+                if (ctrlf.encode("utf-8") in webdata): #ctrl-f the page
+                    print(url)
+                break; #if no errors after checking second time, exit for loop
+            except:
+                continue #keep trying if errors.
+            
+        botnolikey = botnolikey+1 #record number of websites with errors
         continue
 
 print("This many websites didn't like bots:")

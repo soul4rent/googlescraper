1e9bd70b1c1631a1e76021627854989c2a74f4c9
soul4rent <kyle.a.wadsworth@gmail.com>
2018-05-01 10:11:48 -0700

    basic scraping up and running!

--- ggscrape.py
+++ ggscrape.py
@@ -1,6 +1,33 @@
+import time
+from googlesearch import search
 import urllib.request
-url = "http://stackoverflow.com"
-f = urllib.request.urlopen(url)
-test = f.read()
-if (b'Stack' in test):
-    print ("success")
+
+#one of my more major projects that I've worked on
+#url = "https://paintwithbob.com"
+
+#testing url opener with paintwithbob
+#f = urllib.request.urlopen(url)
+#test = f.read()
+#if ("Paint".encode("utf-8") in test):
+#    print (test)
+
+searchterm = input("Search: ")
+ctrlf = input("Find on Page: ")
+
+botnolikey = 0 #keep track of number of no-bot websites
+
+#get top 20 results for reddit
+print("when google searching "+searchterm+", these websites contain the phrase "+ctrlf+" somewhere on the page")
+for url in search(searchterm, stop=20):
+    time.sleep(5) #attempting to fix most "too many requests" errors
+    try: #if the website hates robots
+        f = urllib.request.urlopen(url)
+        webdata = f.read()
+        if (ctrlf.encode("utf-8") in webdata): #ctrl-f the page
+            print(url)
+    except:
+        botnolikey = botnolikey+1
+        continue
+
+print("This many websites didn't like bots:")
+print(botnolikey)
